492403c5a88959a41c80439b06b31a601fd20606
ati333 <stu2301261081@uni-plovdiv.bg>
2025-05-23 22:11:18 +0300

    Първоначален комит: игра на бесеница с React

---
+++ project/hangman/src/main/frontend/src/HangmanGame.jsx
@@ -0,0 +1,43 @@
+const bulgarianAlphabet = [
+    'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й',
+    'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
+    'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ь', 'ю', 'я'
+];
+
+function HangmanGame({ game, setGame }) {
+  const makeGuess = async (letter) => {
+    const gameId = game.id;
+    const response = await fetch(
+      `http://localhost:8090/hangman-games/${gameId}/letter/${letter}`,
+      {
+        method: "PUT",
+      }
+    );
+    const changedGame = await response.json();
+    setGame(changedGame);
+  };
+
+   // Проверка дали играта е приключила
+    const isGameOver = game.status === "WON" || game.status === "LOST";
+
+  return (
+    <>
+      <h2>Player: {game.player.username}</h2>
+      <h2>{game.currentWord}</h2>
+      <h2>{game.status}</h2>
+
+       {/* Показваме клавиатурата само ако играта не е приключила */}
+              {!isGameOver && (
+                <div className="keyboard">
+        bulgarianAlphabet.map((letter) => {
+            return <button onClick={() => makeGuess(letter)}>
+            {letter}
+            </button>
+            ))}
+            </div>
+        )}
+    </>
+  );
+}
+
+export default HangmanGame;
